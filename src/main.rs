d3672b488a5346fe723a2d4203eba17abadf1073
Sycinz <gronbosof@gmail.com>
2024-02-17 22:16:48 +0100

    Fixed code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io;
-use eval::eval;
+use eval::{eval, Value};
 
 fn main() {
-    let mut user_input = String::new();
+    let mut user_input: String = String::from("");
 
     println!("Enter a valid math operation.");
     println!("(for example 12 * 3 + 5)");
@@ -12,7 +12,9 @@ fn main() {
         .read_line(&mut user_input)
         .unwrap();
 
+    let user_input = user_input.trim();
+
     let result = eval(&user_input);
 
-    println!("Here's your result: {:?}", result);
+    println!("Here's your result: {:?}", result); 
 }
